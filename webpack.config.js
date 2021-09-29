948622b3584931d60f872a1147d02434815dc8cf
DilbarAkkaya <dilbarmutavalova@gmail.com>
2021-09-29 04:19:30 +0300

    feat: add links, hovers, change footer...

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const devServer = (isDev) => !isDev ? {} : {
+  devServer: {
+    open: true,
+    hot: true,
+    port: 8080
+  }
+};
+module.exports = ({develop}) => ({
+  mode: develop ? 'development' : 'production',
+  devtool: develop ? 'inline-source-map' : false,
+  entry: {
+  app: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/[hash][ext]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]s$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(?:woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './src/index.html'}),
+    new MiniCssExtractPlugin({filename: '[name].[contenthash].css'}),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname,'src/assets'),
+          to: path.resolve(__dirname,'dist/assets')
+      }
+      ]
+    }),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false}),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  ...devServer(develop)
+});
