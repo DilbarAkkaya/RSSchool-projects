f52918f883c86072839994bd49d44988a3398951
DilbarAkkaya <dilbarmutavalova@gmail.com>
2021-10-20 06:48:11 +0300

    feat: add function showTime

--- momentum/src/index.js
+++ momentum/src/index.js
@@ -1,1 +1,23 @@
 import ('./styles/style.css')
+//------------time----------
+const time = document.querySelector('.time');
+time.textContent = 'Text';
+function showTime() {
+  const date = new Date();
+  hours = date.getHours();
+  minutes = date.getMinutes();
+  seconds = date.getSeconds();
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10){
+  minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  time.textContent = hours + ":" + minutes + ":" + seconds;
+  setTimeout(showTime, 1000);
+}
+showTime();
+
