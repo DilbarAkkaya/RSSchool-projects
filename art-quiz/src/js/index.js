27f1ee5498483f2b52a778d02b0a477bdb8f8d26
DilbarAkkaya <dilbarmutavalova@gmail.com>
2021-11-12 22:07:05 +0300

    feat: add first page html and styles

--- art-quiz/src/js/index.js
+++ art-quiz/src/js/index.js
@@ -1,5 +1,15 @@
 import ('../styles/style.css')
-import ('../styles/owfont-regular.css')
-
 //import playList from './playList.js';
 
+const pages = document.querySelectorAll('.page');
+const btns = document.querySelectorAll('.button');
+
+btns.forEach(btn => btn.addEventListener('click', function() {
+  const pageName = this.dataset.page;
+  this.parentNode.classList.add('hide');
+  pages.forEach(page => {
+    if(page.classList.contains(pageName)) {
+      page.classList.remove('hide')
+    }
+  })
+}))
