b6b73166a7cd876fc6dde9ad16ec94f4f57f2e82
DilbarAkkaya <dilbarmutavalova@gmail.com>
2021-11-14 19:02:18 +0300

    feat: add images

--- art-quiz/src/js/index.js
+++ art-quiz/src/js/index.js
@@ -1,10 +1,12 @@
 import ('../styles/style.css')
-//import playList from './playList.js';
+import images from './images.js';
 
 const pages = document.querySelectorAll('.page');
-const btns = document.querySelectorAll('.button');
+const bloks = document.querySelectorAll('.quiz-blok');
+const btnHome = document.querySelector('.category-btn');
+const pageHome = document.querySelector('.quiz-list');
 
-btns.forEach(btn => btn.addEventListener('click', function() {
+bloks.forEach(blok => blok.addEventListener('click', function() {
   const pageName = this.dataset.page;
   this.parentNode.classList.add('hide');
   pages.forEach(page => {
@@ -13,3 +15,17 @@ btns.forEach(btn => btn.addEventListener('click', function() {
     }
   })
 }))
+
+btnHome.addEventListener('click', function(){
+  this.parentNode.classList.add('hide');
+  pageHome.classList.remove('hide');
+})
+//----создание блоков под раунды-----
+
+let childBlok = document.createElement('div');
+childBlok.className = 'child-blok';
+childBlok.style.backgroundImage = 'url(./assets/img/0.jpg)';
+childBlok.style.backgroundSize = 'cover';
+const categoryBlok = document.querySelector('.category');
+categoryBlok.appendChild(childBlok);
+//------получение изображений из файла
