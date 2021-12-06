1df8ea29c5ea8db9faa0d5f2d9a50abcdb9afc62
DilbarAkkaya <dilbarmutavalova@gmail.com>
2021-12-06 10:26:02 +0300

    fix: fix lint errors

--- migration-typescript/src/components/view/backTop.ts
+++ migration-typescript/src/components/view/backTop.ts
@@ -1,26 +1,25 @@
-
 export function backTop() {
-  let goTopBtn = document.querySelector('.top');
-  function trackScroll() {
-      const scrolled = window.pageYOffset;
-      const coords = document.documentElement.clientHeight;
+    const goTopBtn = document.querySelector('.top');
+    function trackScroll() {
+        const scrolled = window.pageYOffset;
+        const coords = document.documentElement.clientHeight;
 
-      if (scrolled > coords) {
-          goTopBtn.classList.add('top-show');
-      }
-      if (scrolled < coords) {
-          goTopBtn.classList.remove('top-show');
-      }
-  }
+        if (scrolled > coords) {
+            goTopBtn.classList.add('top-show');
+        }
+        if (scrolled < coords) {
+            goTopBtn.classList.remove('top-show');
+        }
+    }
 
-  function backToTop() {
-      if (window.pageYOffset > 0) {
-          window.scrollBy(0, -70);
-          setTimeout(backToTop, 0);
-      }
-  }
+    function backToTop() {
+        if (window.pageYOffset > 0) {
+            window.scrollBy(0, -70);
+            setTimeout(backToTop, 0);
+        }
+    }
 
-  window.addEventListener('scroll', trackScroll);
-  goTopBtn.addEventListener('click', backToTop);
-};
+    window.addEventListener('scroll', trackScroll);
+    goTopBtn.addEventListener('click', backToTop);
+}
 export default backTop;
