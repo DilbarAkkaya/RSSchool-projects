813453cebb86f4742aa54651539c7a963e138412
DilbarAkkaya <dilbarmutavalova@gmail.com>
2021-12-13 02:32:12 +0300

    feat: begin create function to add favorite

--- christmas--task/src/js/index.js
+++ christmas--task/src/js/index.js
@@ -1,43 +1,45 @@
-import ('../styles/style.css')
+import('../styles/style.css')
 import data from './data.js';
 import noUiSlider from 'nouislider/dist/nouislider.mjs';
 
 const sliderCount = document.getElementById('slider-count');
 
 noUiSlider.create(sliderCount, {
-    start: [20, 80],
-    connect: true,
-    range: {
-        'min': 1,
-        'max': 12
-    }
-  })
+  start: [20, 80],
+  connect: true,
+  range: {
+    'min': 1,
+    'max': 12
+  }
+})
 
 const sliderYear = document.getElementById('slider-year');
 
 noUiSlider.create(sliderYear, {
-    start: [20, 80],
-    connect: true,
-    range: {
-        'min': 1940,
-        'max': 2020
-    }
-  })
+  start: [20, 80],
+  connect: true,
+  range: {
+    'min': 1940,
+    'max': 2020
+  }
+})
 const cardBlock = document.querySelector('.card-block');
 
-  function drawToys(data){
-      for (let i = 0; i < data.length; i++) {
-        let card = document.createElement('div');
-        let cardH2 = document.createElement('h2');
-        let cardDescr = document.createElement('div');
-        let count = document.createElement('p');
-        let year = document.createElement('p');
-        let shape = document.createElement('p');
-        let color = document.createElement('p');
-        let size = document.createElement('p');
-        let favorite = document.createElement('p');
+function drawToys(data) {
+  for (let i = 0; i < data.length; i++) {
+    let card = document.createElement('div');
+    let cardH2 = document.createElement('h2');
+    let cardDescr = document.createElement('div');
+    let count = document.createElement('p');
+    let year = document.createElement('p');
+    let shape = document.createElement('p');
+    let color = document.createElement('p');
+    let size = document.createElement('p');
+    let favorite = document.createElement('p');
 
-        card.innerHTML = `
+    data[i].favorite = data[i].favorite ? "Да" : "Нет";
+
+    card.innerHTML = `
         <div class="card" data-num="1">
               <h2 class="card-title">${data[i].name}</h2>
               <img src="/assets/toys/${i+1}.png" alt="toy" class="card-img">
@@ -51,12 +53,19 @@ const cardBlock = document.querySelector('.card-block');
               </div>
               <div class="ribbon"></div>`
 
-        cardDescr.appendChild(count, year, shape, color, size, favorite);
+    cardDescr.appendChild(count, year, shape, color, size, favorite);
 
-  cardBlock.appendChild(card);
-  card.appendChild(cardH2, cardDescr);
+    cardBlock.appendChild(card);
+    card.appendChild(cardH2, cardDescr);
   }
 }
-window.addEventListener("load", function(event) {
+
+window.addEventListener("load", function (event) {
   drawToys(data);
 });
+
+document.onclick = event => {
+  if (event.target.classList.contains('card') && event.target.children[2].children[5].textContent == "Любимая: Да") {
+    console.log(1111111)
+  }
+}
